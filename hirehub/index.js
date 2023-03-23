3e64c6215c3c756ac24f3675401b30a31ba34a5c
Hitesh Yadav <53417879+spyder7370@users.noreply.github.com>
2023-03-23 23:14:20 +0530

    partials

--- hirehub/index.js
+++ hirehub/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let mongoose = require('mongoose');
 let methodOverride = require('method-override');
 let app = express();
-
+let path = require('path');
 mongoose
 	.connect('mongodb+srv://admin:admin@hirehubdb.qt5g4th.mongodb.net/?retryWrites=true&w=majority')
 	.then(function() {
@@ -12,10 +12,11 @@ mongoose
 		console.log(err);
 	});
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+// import router
 let jobRoutes = require('./routes/index.js');
 app.use(jobRoutes);
 
--- hirehub/routes/index.js
+++ hirehub/routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let mongoose = require('mongoose');
 // model
 let Job = require('../models/job-DB.js');
 
@@ -19,14 +20,14 @@ router.get('/jobs', async function(req, res) {
 });
 
 // new
-router.get('/jobs/new', function(req, res) {
+router.get('/jobs/new', async function(req, res) {
 	res.render('new');
 });
 
 // create
 router.post('/jobs', async function(req, res) {
 	try {
-		// make a database object
+		// make a database object / model instance
 		let newJob = new Job({
 			name: req.body.name,
 			address: req.body.address,
@@ -44,7 +45,11 @@ router.get('/jobs/:id', async function(req, res) {
 	try {
 		// fetch the required job by using id
 		let id = req.params.id;
+		let check = mongoose.Types.ObjectId.isValid(id);
 		let job = await Job.findById(id);
+		// eval(require('locus'));
+		// findOne
+		// res.send('test');
 		res.render('show', { job });
 	} catch (error) {
 		console.log('error while fetching a job', error);
@@ -67,12 +72,14 @@ router.get('/jobs/:id/edit', async function(req, res) {
 router.patch('/jobs/:id', async function(req, res) {
 	try {
 		let id = req.params.id;
+		// simple js object
 		let updatedJob = {
 			name: req.body.name,
 			address: req.body.address,
 			image: req.body.image
 		};
 		await Job.findByIdAndUpdate(id, updatedJob);
+		// findOneAndUpdate
 		res.redirect(`/jobs/${id}`);
 	} catch (error) {
 		console.log('error while updating the job', error);
@@ -84,6 +91,7 @@ router.delete('/jobs/:id', async function(req, res) {
 	try {
 		let id = req.params.id;
 		await Job.findByIdAndDelete(id);
+		// findOneAndDestroy
 		res.redirect('/jobs');
 	} catch (error) {
 		console.log('error while deleting the job', error);
